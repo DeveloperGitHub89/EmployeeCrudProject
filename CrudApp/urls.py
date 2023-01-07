2c2cf48fa0341a1dd5c9a0f7405f598165dab9d9
Abhishek Purohit <37204429+DeveloperGitHub89@users.noreply.github.com>
2023-01-07 17:51:56 +0530

    update and delete added

--- CrudApp/urls.py
+++ CrudApp/urls.py
@@ -5,5 +5,8 @@ urlpatterns = [
     path('',views.openIndexPage),
     path('employee-form',views.openEmployeeForm),
     path('save-employee',views.saveEmployee),
-    path('employees-list',views.openEmployeesList)
+    path('employees-list',views.openEmployeesList),
+    path('delete/<int:id>',views.deleteEmployee),
+    path('edit/<int:id>',views.openEditForm),
+    path('update/<int:id>',views.updateEmployee),
 ]
--- CrudApp/views.py
+++ CrudApp/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render,redirect
 from .models import Employee
+from django.http import HttpResponse
 
 # Create your views here.
 def openIndexPage(request):
@@ -20,3 +21,21 @@ def saveEmployee(request):
 def openEmployeesList(request):
     employeesList=Employee.objects.all()
     return render(request,"employees-list.html",{'employees':employeesList})
+
+def deleteEmployee(request,id):
+    emp=Employee.objects.get(id=id)
+    emp.delete()
+    return redirect('/employees-list')
+
+def openEditForm(request,id):
+    emp=Employee.objects.get(id=id)
+    return render(request,"employee-update-form.html",{'employee':emp})
+
+def updateEmployee(request,id):
+    emp=Employee.objects.get(id=id)
+    emp.name=request.POST['name']
+    emp.phone=request.POST['phone']
+    emp.email=request.POST['email']
+    emp.salary=request.POST['salary']
+    emp.save()
+    return redirect('/employees-list')
